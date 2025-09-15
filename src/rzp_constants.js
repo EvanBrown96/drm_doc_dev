7a92787d282c79b475d1086fe77eec2ee90d191c
Evan Brown <eab759@gmail.com>
2025-09-14 20:04:02 -0700

    add 5c4x, 5c6e, 6c6e, fix some data issues

--- src/rzp_constants.js
+++ src/rzp_constants.js
@@ -1,5 +1,5 @@
 const RZPs = [
-    "4c4e", "3c2e", "4c2e", "4c6e", "7c8e", "8c8e", "3c4e", "3c6e"
+    "4c4e", "3c2e", "4c2e", "4c6e", "7c8e", "8c8e", "3c4e", "3c6e", "5c4e", "5c6e", "6c6e"
 ]
 
 export { RZPs };
